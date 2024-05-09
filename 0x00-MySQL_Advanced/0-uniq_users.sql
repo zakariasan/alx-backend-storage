a540010c2e26ed3b16e88857c56646fc9be06e75
zakariasan <elitziko@gmail.com>
2024-05-09 23:16:27 +0100

    mesure my run time async try to hack the world

--- 0x00-MySQL_Advanced/0-uniq_users.sql
+++ 0x00-MySQL_Advanced/0-uniq_users.sql
@@ -1,3 +1,5 @@
+-- Write a SQL script that creates a table users following these requirements:
+
 CREATE TABLE IF NOT EXISTS users (
     id INT AUTO_INCREMENT PRIMARY KEY,
     email VARCHAR(255) NOT NULL UNIQUE,
