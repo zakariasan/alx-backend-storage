797930ff07202a4f847f6050922d5c86f8f8c8e5
zakariasan <elitziko@gmail.com>
2024-05-10 02:18:57 +0100

    Best band ever sql1

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- To list all bands with Glam rock as their main style, ranked by
-SELECT band_name, 
-       IFNULL(DATEDIFF('2022-01-01', SUBSTRING_INDEX(lifespan, '-', 1)), 0)
+SELECT band_name,
+       (IFNULL(SPLIT, 2022), formed)
   AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
--- 0x00-MySQL_Advanced/4-store.sql
+++ 0x00-MySQL_Advanced/4-store.sql
@@ -1,6 +1,6 @@
 -- Write a SQL script that creates a trigger that decreases the quantity
 
-CREATE TRIGGER decrease_quantity_after_order
+CREATE TRIGGER decrement
 AFTER INSERT ON orders
 FOR EACH ROW
 BEGIN
