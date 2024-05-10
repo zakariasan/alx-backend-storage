46d53e37c14c4be7d6ca7154b5bd5118815d48c5
zakariasan <elitziko@gmail.com>
2024-05-10 02:22:38 +0100

    glam rock sql

--- 0x00-MySQL_Advanced/3-glam_rock.sql
+++ 0x00-MySQL_Advanced/3-glam_rock.sql
@@ -1,6 +1,6 @@
 -- To list all bands with Glam rock as their main style, ranked by
 SELECT band_name,
-       (IFNULL(SPLIT, 2022), formed)
+       (IFNULL(SPLIT, 2022) - formed)
   AS lifespan
 FROM metal_bands
 WHERE style LIKE '%Glam rock%'
