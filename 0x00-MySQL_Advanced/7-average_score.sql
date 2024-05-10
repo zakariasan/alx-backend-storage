8df37d545bb1255f2d8901858ede306b50693d4b
zakariasan <elitziko@gmail.com>
2024-05-10 02:35:28 +0100

    averge score

--- 0x00-MySQL_Advanced/7-average_score.sql
+++ 0x00-MySQL_Advanced/7-average_score.sql
@@ -1,25 +1,12 @@
 -- Write a SQL script that creates a trigger that decreases the quantity
 DELIMITER $$
 
-DROP PROCEDURE IF EXISTS ComputeAverageScoreForUser
 CREATE PROCEDURE ComputeAverageScoreForUser(IN user_id INT)
 BEGIN
-    DECLARE total_score FLOAT;
-    DECLARE num_corrections INT;
-    -- Compute total score for the user
-    SELECT SUM(score) INTO total_score
-    FROM corrections
-    WHERE user_id = user_id;
-    -- Compute number of corrections for the user
-    SELECT COUNT(*) INTO num_corrections
-    FROM corrections
-    WHERE user_id = user_id;
-    -- Compute average score
-    IF num_corrections > 0 THEN
-        UPDATE users
-        SET average_score = total_score / num_corrections
-        WHERE id = user_id;
-    END IF;
+  UPDATE users
+  SET
+  average_score = (SELECT AVG(score) FROM corrections where corrections.user_id = user_id)
+  where id = user_id
 END $$
 
 DELIMITER ;
