63954359805a41cde5d48c1929eef531aee8f0e1
zakariasan <elitziko@gmail.com>
2024-05-15 01:15:25 +0100

    log stats11

--- 0x01-NoSQL/102-log_stats.py
+++ 0x01-NoSQL/102-log_stats.py
@@ -8,17 +8,26 @@ from pymongo import MongoClient
 if __name__ == "__main__":
     """ try to log in """
     client = MongoClient('mongodb://127.0.0.1:27017')
-    logs_collection = client.logs.nginx
+    logs_collection = client.server_logs.nginx_access_logs
 
-    total = logs_collection.count_documents({})
-    methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
-    counts = {method: logs_collection.count_documents(
-        {"method": method}) for method in methods}
-    path_count = logs_collection.count_documents({"method": "GET", "path":
-                                                  "/status"})
+    total_logs = logs_collection.count_documents({})
+    http_methods = ["GET", "POST", "PUT", "PATCH", "DELETE"]
+    method_counts = {method: logs_collection.count_documents(
+        {"method": method}) for method in http_methods}
+    status_check_count = logs_collection.count_documents(
+        {"method": "GET", "path": "/status"})
 
-    print(f"{total} logs")
-    print("Methods:")
-    for method, count in counts.items():
-        print(f"\tmethod {method}: {count}")
-    print(f"{path_count} status check")
+    print(f"Total logs: {total_logs}")
+    print("HTTP Methods:")
+    for method, count in method_counts.items():
+        print(f"\t{method}: {count}")
+    print(f"Status check: {status_check_count}")
+
+    print("Top 10 IPs:")
+    top_ips = logs_collection.aggregate([
+        {"$group": {"_id": "$ip", "count": {"$sum": 1}}},
+        {"$sort": {"count": -1}},
+        {"$limit": 10}
+    ])
+    for i, ip_info in enumerate(top_ips, start=1):
+        print(f"\t{i}. {ip_info['_id']}: {ip_info['count']}")
