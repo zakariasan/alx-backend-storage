c1cf59025d113316badea7e3b32ea823666f9f2c
zakariasan <elitziko@gmail.com>
2024-05-16 03:14:29 +0100

    implementing web py

--- 0x02-redis_basic/web.py
+++ 0x02-redis_basic/web.py
@@ -2,14 +2,16 @@
 """ Redis client module
 """
 import requests
-import redis
 from functools import wraps
+import redis
 
 
 def cache_page(func):
     """ cache page """
     @wraps(func)
-    def wrapper(url):
+    def wrapper(url: str) -> str:
+        """ wrapper the wrapper """
+        cache = redis.Redis()
         key = f"count:{url}"
         content = cache.get(key)
         if content:
@@ -18,14 +20,13 @@ def cache_page(func):
         response = func(url)
         cache.setex(key, 10, response)
         return response
-
     return wrapper
 
 
+@cache_page
 def get_page(url: str) -> str:
     """ get page str """
     response = requests.get(url)
     return response.text
 
 
-cache = redis.Redis()
