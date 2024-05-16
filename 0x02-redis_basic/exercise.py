621ead0d2b4b9025b1756c5a7b02465b67cd4680
zakariasan <elitziko@gmail.com>
2024-05-16 02:41:14 +0100

    log stats11

---
+++ 0x02-redis_basic/exercise.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+""" Redis client module
+"""
+import redis
+import uuid
+from typing import Union
+
+
+class Cache:
+    """ class caching"""
+
+    def __init__(self):
+        self._redis = redis.Redis()
+        self._redis.flushdb()
+
+    def store(self, data: Union[str, bytes, int, float]) -> str:
+        key = str(uuid.uuid4())
+        self._redis.set(key, data)
+        return key
